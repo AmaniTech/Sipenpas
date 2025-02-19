47287e5e80b5bd85d15067d9cb10f62653ebfbb3
Rpebs pramesta <rpebspramesta@gmail.com>
2025-02-19 10:51:31 +0700

    feat: add store and update logic kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -15,15 +15,49 @@ class KategoriController extends Controller
         return view('master.kategori.index')->with($data);
     }
 
-    public function ajaxData()
+    public function store(Request $request)
     {
-        $query = Kategori::orderBy('id', 'desc');
-        return DataTables::of($query)
-            ->addColumn('action', function ($item) {
-                return '<a href="#" class="btn btn-sm btn-primary">Edit</a>' . ' ' . '<a href="#" class="btn btn-sm
-                btn-danger">Hapus</a>';
-            })
-            ->rawColumns(['action'])
-            ->make(true);
+        try {
+            $request->validate([
+                'nama' => 'required',
+            ]);
+
+            Kategori::create([
+                'nama' => $request->nama,
+            ]);
+
+            return redirect()->route('kategori.index')->with('success', 'Data berhasil ditambahkan');
+        } catch (\Exception $e) {
+            return redirect()->route('kategori.index')->with('error', 'Data gagal ditambahkan');
+        }
     }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $request->validate([
+                'nama' => 'required',
+            ]);
+
+            Kategori::find($id)->update([
+                'nama' => $request->nama,
+            ]);
+
+            return redirect()->route('kategori.index')->with('success', 'Data berhasil diubah');
+        } catch (\Exception $e) {
+            return redirect()->route('kategori.index')->with('error', 'Data gagal diubah');
+        }
+    }
+
+    // public function ajaxData()
+    // {
+    //     $query = Kategori::orderBy('id', 'desc');
+    //     return DataTables::of($query)
+    //         ->addColumn('action', function ($item) {
+    //             return '<a href="#" class="btn btn-sm btn-primary">Edit</a>' . ' ' . '<a href="#" class="btn btn-sm
+    //             btn-danger">Hapus</a>';
+    //         })
+    //         ->rawColumns(['action'])
+    //         ->make(true);
+    // }
 }
